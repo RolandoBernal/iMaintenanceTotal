966b220c1bb415e307f0d0a11d1e9988d41f8e0e
Lando B <jrolandobernal@gmail.com>
2015-11-22 23:05:18 -0600

    Added CSharp project

---
+++ iMaintenanceTotal/iMaintenanceTotal/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace iMaintenanceTotal
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ iMaintenanceTotal/iMaintenanceTotal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iMaintenanceTotal.Startup))]
+namespace iMaintenanceTotal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
