b4b1c6a779acd28af8b0b780b3d1ab8fd34b45d1
Lando B <jrolandobernal@gmail.com>
2015-11-30 17:19:18 -0600

    Added UnitTest files

---
+++ iMaintenanceTotal/iMaintenanceTotal.Tests/MaintTaskRepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace iMaintenanceTotal.Tests
+{
+    [TestClass]
+    public class MaintTaskRepositoryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ iMaintenanceTotal/iMaintenanceTotal.Tests/MaintTaskTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace iMaintenanceTotal.Tests
+{
+    [TestClass]
+    public class MaintTaskTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
