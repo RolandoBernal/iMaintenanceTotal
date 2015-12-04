ef0f20ee88f9a6a9531a5f02db98b8a75a39eeb0
Lando B <jrolandobernal@gmail.com>
2015-12-04 08:02:33 -0600

    Added MaintRepositoryTests.cs

--- iMaintenanceTotal/iMaintenanceTotal.Tests/MaintTaskRepositoryTests.cs
+++ iMaintenanceTotal/iMaintenanceTotal.Tests/MaintTaskRepositoryTests.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Moq;
+//using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace iMaintenanceTotal.Tests
 {
--- iMaintenanceTotal/iMaintenanceTotal/Models/MaintTask.cs
+++ iMaintenanceTotal/iMaintenanceTotal/Models/MaintTask.cs
@@ -7,5 +7,26 @@ namespace iMaintenanceTotal.Models
 {
     public class MaintTask
     {
+        public int MaintTaskId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime CompleteBy { get; set; }
+        public bool Completed { get; set; }
+        public string Repeat { get; set; }
+        public DateTime RemindMeOn { get; set; }
     }
 }
+
+/*
+*****MaintTask*****
+TaskID: int
+Title: string
+Description: string
+CreatedAt: date
+DueDate: date
+Completed: bool
+Frequency: date
+Reminder: date
+Category: string
+*/
