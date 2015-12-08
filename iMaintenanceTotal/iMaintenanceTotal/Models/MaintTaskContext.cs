f49ab8aac21a72ade7d5a321452b856068d88886
Lando B <jrolandobernal@gmail.com>
2015-12-07 21:16:26 -0600

    Created MaintTaskController.cs, MaintTaskContext.cs, MaintTaskRepository.cs

---
+++ iMaintenanceTotal/iMaintenanceTotal/Models/MaintTaskContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace iMaintenanceTotal.Models
+{
+    public class MaintTaskContext : DbContext
+    {
+        public IDbSet<ApplicationUser> Users { get; internal set; }
+    }
+}
---
+++ iMaintenanceTotal/iMaintenanceTotal/Models/MaintTaskRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace iMaintenanceTotal.Models
+{
+    public class MaintTaskRepository
+    {
+        private MaintTaskContext context;
+
+        // DbContext is now ApplicationDbContext which gives use access to the
+        // table containing the users.
+        public IDbSet<ApplicationUser> Users { get { return context.Users; } }
+
+        public MaintTaskRepository()
+        {
+            context = new MaintTaskContext();
+        }
+
+        public MaintTaskRepository(MaintTaskContext _context)
+        {
+            context = _context;
+        }
+    }
+}
