e31720bbdc9fc4ba32af060b695e5f60ad7c4e5a
Lando B <jrolandobernal@gmail.com>
2015-12-07 18:29:54 -0600

    Re-Created project. Added MaintTask.cs class. Added MaintTaskTests.cs Unit Test. Added a simple test. It passed.

---
+++ iMaintenanceTotal/iMaintenanceTotal.Tests/Models/MaintTaskTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using iMaintenanceTotal.Models;
+
+namespace iMaintenanceTotal.Tests.Models
+{
+    [TestClass]
+    public class MaintTaskTests
+    {
+        [TestMethod]
+        public void MaintTaskEnsureICanCreateAnInstance()
+        {
+            MaintTask m_task = new MaintTask();
+            Assert.IsNotNull(m_task);
+        }
+
+        
+    }
+}
