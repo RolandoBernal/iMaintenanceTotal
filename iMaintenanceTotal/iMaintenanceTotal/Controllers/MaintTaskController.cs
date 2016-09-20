24616c0cb6a3170206a79d0c22e6b941bd096831
Lando B (Rolando Bernal) <jrolandobernal@gmail.com>
2016-09-20 13:22:40 -0500

    Refactoring

--- iMaintenanceTotal/iMaintenanceTotal/Controllers/MaintTaskController.cs
+++ iMaintenanceTotal/iMaintenanceTotal/Controllers/MaintTaskController.cs
@@ -1,10 +1,7 @@
 ﻿using iMaintenanceTotal.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using System.Data.Entity;
 using System.Net;
